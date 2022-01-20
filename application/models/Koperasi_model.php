0fa2a1e2b276df7d50ab5773f709b5e405905e70
FadhilSkylake <fadhil1023@gmail.com>
2022-01-20 15:00:56 +0700

    commit

---
+++ application/models/Koperasi_model.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class Koperasi_model extends CI_Model 
+{
+    public function select()
+    {
+
+    }                        
+                        
+}
+
+
+/* End of file Koperasi_model.php and path \application\models\Koperasi_model.php */
--- application/views/koperasi.php
+++ application/views/koperasi.php
@@ -3,25 +3,27 @@
       <h3 class="card-title">DataTable with default features</h3>
     </div>
     <div class="card-body">
-      <table id="example1" class="table table-bordered table-striped">
-        <thead>
-        <tr>
-          <th>Rendering engine</th>
-          <th>Browser</th>
-          <th>Platform(s)</th>
-          <th>Engine version</th>
-          <th>CSS grade</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Trident</td>
-          <td>Internet
-            Explorer 4.0
-          </td>
-          <td>Win 95+</td>
-          <td> 4</td>
-          <td>X</td>
-        </tr>
+        <table id="example1" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Rendering engine</th>
+                    <th>Browser</th>
+                    <th>Platform(s)</th>
+                    <th>Engine version</th>
+                    <th>CSS grade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                        Explorer 4.0
+                    </td>
+                    <td>Win 95+</td>
+                    <td> 4</td>
+                    <td>X</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
