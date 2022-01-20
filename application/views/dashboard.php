6f5aa4e544a34595e7466b087bc66ba2109f4de8
kurosagi122 <riyadhfauzan12@gmail.com>
2022-01-20 14:26:08 +0700

    commit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,1 +1,97 @@
-Dashboard
+<!-- Main content -->
+<section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>426</h3>
+                <p>Koperasi</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-balance-scale"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>123<sup style="font-size: 20px"></sup></h3>
+
+                <p>Perdagangan</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-chart-line"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p>Industri</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-industry"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>19000</h3>
+
+                <p>UMKM</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-cookie-bite"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Pasar</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-shopping-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Sekretariat</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-users"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+</section>
+
+
+    
+
