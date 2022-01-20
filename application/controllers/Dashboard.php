d4b50686dd49a9b3f2b74e6d4c8c43436990a02a
FadhilSkylake <fadhil1023@gmail.com>
2022-01-20 13:41:12 +0700

    commit

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,8 @@ class Dashboard extends CI_Controller {
 
     public function index()
     {
+        $data['title'] = 'Dashboard';
+
         $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
         $this->load->view('dashboard');
