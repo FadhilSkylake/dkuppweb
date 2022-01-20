712bc393a45bac382370f5f748ea14bfd19ab881
FadhilSkylake <fadhil1023@gmail.com>
2022-01-20 13:12:10 +0700

    commit

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -43,11 +43,47 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>Dashboard</p>
                 </a>
-              </li>
+          </li>
+          <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Koperasi</p>
+                </a>
+          </li>
+          <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Perdagangan</p>
+                </a>
+          </li>
+          <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Industri</p>
+                </a>
+          </li>
+          <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>UMKM</p>
+                </a>
+          </li>
+          <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Pasar</p>
+                </a>
+          </li>
+          <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Sekretariat</p>
+                </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
