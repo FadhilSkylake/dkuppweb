34d5203be384cc4f6fa392b214b9d2f56493cfbc
FadhilSkylake <fadhil1023@gmail.com>
2022-01-20 13:32:05 +0700

    commit

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -44,13 +44,13 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
                 <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
                   <p>Dashboard</p>
                 </a>
           </li>
           <li class="nav-item">
                 <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                <i class="fas fa-balance-scale nav-icon"></i>
                   <p>Koperasi</p>
                 </a>
           </li>
@@ -62,7 +62,7 @@
           </li>
           <li class="nav-item">
                 <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                <i class="fas fa-industry nav-icon"></i>
                   <p>Industri</p>
                 </a>
           </li>
