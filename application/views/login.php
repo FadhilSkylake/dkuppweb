e3684e22397e7dfc6fc5de03d4e8e69347438dd4
FadhilSkylake <fadhil1023@gmail.com>
2022-01-21 21:38:12 +0700

    commit

--- application/views/login.php
+++ application/views/login.php
@@ -1,17 +1,17 @@
-<body class="hold-transition login-page">
+<body class="hold-transition login-page text-center">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>DKUPP SUBANG</b></a>
+            <img src="<?= base_url('assets/template/') ?>/img/logo.png" class="mx-auto d-block" style="max-width:100px; margin-top: -7px;">
+            <a><b>DKUPP SUBANG</b></a>
             <h5>Dinas Koperasi UMKM Perdagangan Perindustrian & Pasar</h5>
         </div>
         <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-
+                <p class="login-box-msg">LOGIN</p>
                 <form action="../../index3.html" method="post">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="username" class="form-control" placeholder="Username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -26,15 +26,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
+                    <div class="row d-flex justify-content-center">
                         <!-- /.col -->
                         <div class="col-4">
                             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
@@ -42,32 +34,15 @@
                         <!-- /.col -->
                     </div>
                 </form>
-
-                <div class="social-auth-links text-center mb-3">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-block btn-primary">
-                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                    </a>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                    </a>
-                </div>
-                <!-- /.social-auth-links -->
-
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
-                </p>
             </div>
-            <!-- /.login-card-body -->
         </div>
-    </div>
-    <!-- jQuery -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
+        <!-- jQuery -->
+        <script src="../../plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="../../dist/js/adminlte.min.js"></script>
+        <footer>
+            <strong>Copyright &copy; DKUPP SUBANG</strong>
+        </footer>
 </body>
