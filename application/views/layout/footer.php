5b00568bfb6f38825f5e75a2d593acd47c3cafc1
FadhilSkylake <fadhil1023@gmail.com>
2022-01-23 17:12:01 +0700

    commit

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,29 +1,29 @@
 <footer class="bg-success text-white text-left pb-3">
-      <section id="contact">
-        <div class="container">
-          <div class="row text-center mb-3">
-            <div class="col">
-              <h2>Contact Us</h2>
-            </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-md-10">
-              <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-              </div>
-              <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Nama</label>
-                <input type="nama" class="form-control" id="exampleFormControlInput1" />
-              </div>
-              <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Komentar</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-          </div>
+  <section id="contact">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>Contact Us</h2>
         </div>
-      </section>
-        <p class="text-center">Created by : Magang UNIVERSITAS SUBANG</p>
-    </footer>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Nama</label>
+            <input type="nama" class="form-control" id="exampleFormControlInput1" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Komentar</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <p class="text-center">Created by : Magang UNIVERSITAS SUBANG</p>
+</footer>
--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,3 +1,13 @@
+<link rel="stylesheet" href="<?= base_url('assets/template') ?>/plugins/fontawesome-free/css/all.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- DataTables -->
+<link rel="stylesheet" href="<?= base_url('assets/template') ?>/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= base_url('assets/template') ?>/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="<?= base_url('assets/template') ?>/dist/css/adminlte.min.css">
+<!-- Google Font: Source Sans Pro -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
   <div class="container">
     <img src="<?= base_url('assets/img') ?>/logo.png" alt="" width="70" height="50">
