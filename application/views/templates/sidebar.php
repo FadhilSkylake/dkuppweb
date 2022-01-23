72320321e00f49ff439a7151102a0ddf5550a5a4
FadhilSkylake <fadhil1023@gmail.com>
2022-01-23 22:58:39 +0700

    commit

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -8,11 +8,9 @@
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
+
         <li class="nav-item d-none d-sm-inline-block">
-          <a href="index3.html" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Contact</a>
+          <span class="brand-text font-weight-light">Dinas Koperasi UMKM <br> Perdagangan Perindustrian <br>dan Pasar</span>
         </li>
       </ul>
     </nav>
@@ -23,20 +21,19 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
-        <img src="<?= base_url('assets/template/') ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Subang Jawara</span>
+      <a href="#" class="brand-link">
+        <img src="<?= base_url('assets') ?>/img/logo.png" width="50px" height="60px">
       </a>
-
       <!-- Sidebar -->
       <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
+        <span class="brand-text font-weight-bold">
+          <br>
+          <a class="d-block">Login Sebagai <?php echo $this->session->userdata('akun'); ?></a>
+        </span>
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="<?= base_url('assets/template/') ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-          </div>
+
           <div class="info">
-            <a class="d-block"><?php echo $this->session->userdata('akun'); ?></a>
             <a href="<?php echo base_url() ?>login/logout">Logout</a>
           </div>
         </div>
