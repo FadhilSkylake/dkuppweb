1df54ddb9567cbe5c4bdce711b835ccebfc09915
FadhilSkylake <fadhil1023@gmail.com>
2022-01-26 07:39:45 +0700

    commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,14 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Dashboard_model');
+    }
     public function index()
     {
+        $this->data['total_koperasi'] =  $this->Dashboard_model->total_koperasi();
+        $this->data['total_pasar'] =  $this->Dashboard_model->total_pasar();
+        $this->data['total_perdagangan'] =  $this->Dashboard_model->total_perdagangan();
+        $this->data['total_industri'] =  $this->Dashboard_model->total_industri();
+        $this->data['total_umkm'] =  $this->Dashboard_model->total_umkm();
         $data['judul'] = 'Home';
 
         $this->load->view('layout/header', $data);
         $this->load->view('layout/navbar', $data);
-        $this->load->view('front/home');
+        $this->load->view('front/home', $this->data);
         $this->load->view('layout/footer');
     }
 }
